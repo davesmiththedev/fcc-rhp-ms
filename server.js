7ec0a58c4edda1de99ac90579a3feffdee0bf6b9
Dave Smith <lah4life@gmail.com>
2017-01-28 03:24:44 +0000

    Add functions to server.js to parse and return header data

--- server.js
+++ server.js
@@ -5,9 +5,26 @@ var port = process.env.PORT || 8080;
 
 app.get('/', (req, res)=>{
     var ip = req.header('x-forwarded-for');
-    var userAgent = req.header('user-agent');
-    var acceptLanguage = req.header('accept-language');
+    var os = getOS(req.header('user-agent'));
+    var language = getLanguage(req.header('accept-language'));
     
-    res.end(ip + ' - ' + userAgent + ' - ' + acceptLanguage);
+    res.end(ip + ' - ' + os + ' - ' + language);
     
 }).listen(port);
+
+function getLanguage(acceptLanguage){
+    if (acceptLanguage != null){
+        var langaugeData = acceptLanguage.split(',');
+        return langaugeData[0];
+    }else{
+        return null;
+    }
+};
+
+function getOS(userAgent){
+    var firstSplit = userAgent.split('(');
+    var secondSplit = firstSplit[1].split(';');
+    var os = secondSplit[0];
+    
+    return os;
+};
